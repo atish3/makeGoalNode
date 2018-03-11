b84bdff001f52a1a02fa730a2e95a5edc559f809
Atishay Singh <32619815+atish3@users.noreply.github.com>
2018-03-11 17:24:23 -0400

    Add files via upload

---
+++ makeGoal.cpp
@@ -0,0 +1,42 @@
+#include "ros/ros.h"
+#include <move_base_msgs/MoveBaseAction.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include "std_msgs/String.h"
+#include "std_msgs/UInt16.h"
+
+class MakeGoal {
+
+public:
+	MakeGoal();
+private:
+    double x;
+    double w;
+    move_base_msgs::MoveBaseGoal goal;
+	ros::Subscriber sub = n.subscribe("txt", 1000, readIn);
+    ros::Publisher output_pub = n.advertise<move_base_msgs::MoveBaseGoal>("simple_goal", 1000);
+	void makeAGoal(const double &x, const double &w, move_base_msgs::MoveBaseGoal goal);
+    void readIn(istream &txt, double &x, double &w);
+};
+
+void MakeGoal::makeAGoal(const double &x, const double &w, move_base_msgs::MoveBaseGoal goal){
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
+    goal.target_pose.pose.position.x = x;
+    goal.target_pose.pose.orientation.w = w;
+    output_pub.publish(goal);
+}
+
+void MakeGoal::readIn(istream &txt, double &x, double &w){
+    while(txt >> x >> w){
+        this.makeAGoal(x, w, goal);
+    }
+}
+
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "output");
+	MakeGoal make_goal;
+	ros::NodeHandle n;
+	ros::spin();
+	return 0;
+}
